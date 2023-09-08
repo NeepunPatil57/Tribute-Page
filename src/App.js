b46d2d699a7c0a0d686f56583b83aa9ff3fd6c98
Neepun Patil <pneepun@gmail.com>
2023-09-08 20:47:15 +0530

    Completed
    Completed

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Main-content">
+        <img
+          src="https://www.skillpundit.com/img/freedom-fighters/skillpundit-gandhi.jpg"
+          alt="Mahatma Gandhi"
+          className="Gandhi-image"
+        />
+        <h2 className="title">Mahatma Gandhi</h2>
+        <p>
+          Mohandas Karamchand Gandhi, popularly known as Mahatma Gandhi, was a
+          prominent leader in the Indian independence movement against British
+          colonial rule. He advocated for nonviolent civil disobedience and played
+          a key role in India's struggle for freedom.
+        </p>
+        <h3>Early Life and Education</h3>
+        <p>
+          Gandhi was born on October 2, 1869, in Porbandar, India. He studied law
+          in London and eventually became a lawyer in South Africa. It was during
+          his time in South Africa that he developed his philosophy of nonviolent
+          resistance, which he called Satyagraha.
+        </p>
+        <h3>Salt March and Civil Disobedience</h3>
+        <p>
+          One of Gandhi's most significant acts of civil disobedience was the Salt
+          March in 1930, where he and his followers walked to the Arabian Sea to
+          make their own salt in protest of British salt taxes. This act of
+          defiance gained international attention and became a symbol of India's
+          fight for independence.
+        </p>
+        <h3>Legacy and Impact</h3>
+        <p>
+          Mahatma Gandhi's philosophy of nonviolence and civil disobedience
+          inspired movements for civil rights and freedom across the world. He
+          worked tirelessly to unite different religious and ethnic communities in
+          India. His efforts culminated in India gaining independence from British
+          rule on August 15, 1947.
+        </p>
+        <h3>Assassination and Rememberance</h3>
+        <p>
+          Tragically, Mahatma Gandhi was assassinated on January 30, 1948, by a
+          Hindu nationalist who opposed his views. However, his legacy lives on as
+          an icon of peace, justice, and human rights.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
